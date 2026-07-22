24b98bd56fe996cc72c39eae8cc133aa6094587d
nihaowocuo <2746582407@qq.com>
2026-07-23 00:02:20 +0800

    此次学习了for循环语法

--- py_code/交互.py
+++ py_code/交互.py
@@ -39,9 +39,34 @@
 #     sum = sum + i
 #     i = i + 1
 # print(sum)
-while True:
-    content = input("请输入你要说的话：")
-    if content == "quit":
-        break
-    print("发送给他人：",content)
+#     注意：while常用于死循环
+# while True:
+#     content = input("请输入你要说的话：")
+#     if content == "quit":
+#         break
+#     print("发送给他人：",content)
+# for循环的语法：
+# py中for循环不同于我此前学习到的语言，其可以
+# for 变量 in 可迭代的对象:
+#     代码
+#     将可迭代的每一项东西都拿出来，赋值一遍给变量
+#     我此前接触最多的是数字，下方的例子为字符串
+# name = "垂杨，你好啊"
+# for str in name:
+#     print("本次输出的为：",str)
+# for 循环计数采用 range
+#     注意：以下均不包含n
+#     # 从零数到n
+#     range(n):
+#     # 从m数到n
+#     range(m,n):
+#     # 从m数到n，中间间隔t
+#     range(m,n,t):
+# for i in range(10):
+#     print(i)
 
+# for i in range(2,10):
+#     print(i)
+
+# for i in range(2,10,2):
+#     print(i)
