65f04c029ccbead5a281c46d1ccf7a0559629719
nihaowocuo <2746582407@qq.com>
2026-07-26 23:26:51 +0800

    字符串基础学习

--- py_code/second.py
+++ py_code/second.py
@@ -10,3 +10,36 @@
 #     bytes
 #     运算符
 #     文件操作
+# int 整型 加减乘除 大小比较
+# float 浮点型 同上 但是小数是无限的，但计算机能表示的范围有限
+# bool 布尔类型 条件判断 仅有 true false
+# type 可以用来测试类型
+# a = 10
+# print(a)
+# print(type(a))
+# 在py中所有的非零数字均为true
+# 仅有0表示false
+# a = 10
+# b = bool(a)
+# print(type(a))
+# print(type(b))
+# print(a)
+# print(b)
+# while True:死循环
+# 而在字符串中
+# 同样所有非空字符串均为true，空字符串为false
+# 即，在py中空即为false，非空即为true
+# lst = [13]
+# print(bool(lst))
+# lst_1 = []
+# print(bool(lst_1))
+# lst_2 = [0]
+# print(bool(lst_2))
+# 也可以用空与非空表判断
+# while 1:
+#     content = input("请输入指示：")
+#     if content :
+#         print("来吧：",content)
+#     else:
+#         break
+
